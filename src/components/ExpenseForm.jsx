2cb25a56e7a4ae8c88e375745230ada2308f8c43
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-10-06 20:10:56 +0530

    implement the unidirectional data flow or one way data binding to update the screen/DOM while updation in the state

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ExpenseForm({ setExpenses }) {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();  // it stops from page reload
-    // console.log(e.target);
-    const expense = { ...getFormData(e.target), id: crypto.randomUUID() }
+    const expense = { title, category, amount, id: crypto.randomUUID() };
     setExpenses((prevState) => [...prevState, expense]);
-    e.target.reset();  // to clear the previous data from the input fields in the form
+    // e.target.reset();  // it don't work here due to one way data binding we don't update the screen until we don't update the state. (Unidirectional Data Flow)
+    setTitle("");
+    setCategory("");
+    setAmount("");
   }
+  
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();  // it stops from page reload
+  //   // console.log(e.target);
+  //   // const expense = { ...getFormData(e.target), id: crypto.randomUUID() }
+  //   // setExpenses((prevState) => [...prevState, expense]);
+  //   // e.target.reset();  // to clear the previous data from the input fields in the form
+  // }
 
-  const getFormData = (form) => {
-    const formData = new FormData(form)
-    const data = {}
-    // for (const value of formData.values()) {  // values only gives value
-    for (const [key, value] of formData.entries()) {  // entries gives both value and id
-      data[key] = value
-    }
-    return data;
-  }
+  // const getFormData = (form) => {
+  //   const formData = new FormData(form)
+  //   const data = {}
+  //   // for (const value of formData.values()) {  // values only gives value
+  //   for (const [key, value] of formData.entries()) {  // entries gives both value and id
+  //     data[key] = value
+  //   }
+  //   return data;
+  // }
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" />
+        <input id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" name="category">
+        <select id="category" name="category" value={category} onChange={(e) => setCategory(e.target.value)}> 
           <option value="" hidden>
             Select Category
           </option>
@@ -40,7 +54,7 @@ export default function ExpenseForm({ setExpenses }) {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" />
+        <input id="amount" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
       </div>
       <button className="add-btn">Add</button>
     </form>
